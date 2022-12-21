9a30c098a156ee9d808cee9ff549ad027bce9545
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-12-21 15:22:41 +0500

    Initial Commit

---
+++ SberWebViewTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SberWebViewTesting
+//
+//  Created by Mikhail Rubanov on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
